dd397b887917b747dc3c60a21ca7107f5fea069f
Alien Coder <59464137+sehoon787@users.noreply.github.com>
2021-12-22 00:29:15 +0900

    Update ScissorsGUI.py

--- ScissorsGUI.py
+++ ScissorsGUI.py
@@ -71,6 +71,8 @@ class PlanExtractor(QMainWindow, mainDlg_class):
 
     def startBtnFunction(self):
         try:
+            chdir(self.default_path)
+
             self.progressBar.setValue(0)
             self.progressBar.setMaximum(len(self.loadList))
             filecnt = 0
@@ -93,12 +95,13 @@ class PlanExtractor(QMainWindow, mainDlg_class):
 
                 # Make directory to save result files
                 try:
-                    imwrite('../result/'+newName, result)
+                    chdir("../")
+                    imwrite('./result/'+newName, result)
                 except Exception as e:
-                    if "[WinError 2] 지정된 파일을 찾을 수 없습니다: '../result/'" in str(e) or\
+                    if "[WinError 2] 지정된 파일을 찾을 수 없습니다: './result/'" in str(e) or\
                             "No such file or directory" in str(e):
-                        mkdir('../result')
-                    imwrite('../result/' + newName, result)
+                        mkdir('./result')
+                    imwrite('./result/' + newName, result)
 
                 self.textEdit_jpgList.setText(currentState)
 
